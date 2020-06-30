324f099d2c69e2ec140bfc6116484f36bf41a0dd
Samuel Simões <samuelsimoes31@gmail.com>
2020-06-30 17:41:04 -0300

    feat(navigation): NavigationContainer created

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { View } from 'react-native'; //(dev only) just to have a valid react component
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" headerMode="none">
+        <Stack.Screen name="Home" component={View} />
+        <Stack.Screen name="ZeClub" component={View} />
+        <Stack.Screen name="ChatBot" component={View}/>
+        <Stack.Screen name="QrCode" component={View}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
