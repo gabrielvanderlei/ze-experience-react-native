e7dd0ddebda5005b6bd6c0d5a3fb0ca0ed77bf6f
Gabriel Vanderlei <gabrielvanderlei10@hotmail.com>
2020-07-03 20:55:10 -0300

    feat(server/mongo): add mongodb connection to the server
    Installed the mongodb connection to server and the chat route.

--- server/index.js
+++ server/index.js
@@ -8,8 +8,27 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors());
 
+
+mongoose.connect('mongodb://localhost/ze-experience', {useNewUrlParser: true});
+
+var chatSchema = new mongoose.Schema({
+  message: String,
+  step: String,
+  location: String,
+  clientId: String,
+}, { timestamps: true });
+
+var ChatModel = mongoose.model('Chat', chatSchema);
+
 app.get('/', (req, res) => {
   res.json({  message: "Server running." });
+})
+
+app.post('/message', (req, res) => {
+  const { data } = req.body;
+  ChatModel.create(data); 
+  res.json({ success: true });
+  console.log(`Received chat information: ${+(new Date())}`);
 })
 
 app.listen(3000, function(){
